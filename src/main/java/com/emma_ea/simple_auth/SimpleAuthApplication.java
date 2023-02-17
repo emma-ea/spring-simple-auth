ed3ab44ed0b9d74968a78e7ef9055d9dfe625bed
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2023-02-17 16:08:35 +0000

    initial commit

---
+++ src/main/java/com/emma_ea/simple_auth/SimpleAuthApplication.java
@@ -0,0 +1,13 @@
+package com.emma_ea.simple_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleAuthApplication.class, args);
+	}
+
+}
