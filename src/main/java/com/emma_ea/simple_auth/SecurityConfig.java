e03392718f7e6838f53fa3eff1ac6b7f12cdabb2
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2023-02-17 17:56:02 +0000

    basic auth impl spring + thymeleaf

---
+++ src/main/java/com/emma_ea/simple_auth/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.emma_ea.simple_auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf()
+                .disable()
+                .authorizeRequests().antMatchers("/", "/home").permitAll()
+                .and().authorizeRequests().anyRequest().authenticated()
+                .and().formLogin(form -> form.loginPage("/login").permitAll())
+                .logout(LogoutConfigurer::permitAll);
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User
+                .withUsername("user")
+                .password(passwordEncoder().encode("password"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+}
--- src/main/java/com/emma_ea/simple_auth/SimpleAuthApplication.java
+++ src/main/java/com/emma_ea/simple_auth/SimpleAuthApplication.java
@@ -2,6 +2,7 @@ package com.emma_ea.simple_auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 public class SimpleAuthApplication {
